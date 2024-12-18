35be2af88dc14a09fbe34e478f77cb6022a591cf
Spit365 <lukas.grossauer47@gmail.com>
2024-12-18 18:00:59 +0100

    1st commit

---
+++ src/main/java/net/spit365/currency/CurrencyDataGenerator.java
@@ -0,0 +1,11 @@
+package net.spit365.currency;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CurrencyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
